a88e0a18233d75881302369365a38803eedc506f
guodong.hu <hgd250@gmail.com>
2023-09-22 19:40:44 +0800

    zh->en

--- src/locales/lang/zh_CN.ts
+++ src/locales/lang/zh_CN.ts
@@ -1,10 +1,12 @@
 import { genMessage } from '../helper';
-import antdLocale from 'ant-design-vue/es/locale/zh_CN';
+import antdLocale from 'ant-design-vue/es/locale/en_US';
 
-const modules = import.meta.glob('./zh-CN/**/*.ts', { eager: true });
+const modules = import.meta.glob('./en/**/*.ts', { eager: true });
 export default {
   message: {
-    ...genMessage(modules as Recordable<Recordable>, 'zh-CN'),
+    ...genMessage(modules as Recordable<Recordable>, 'en'),
     antdLocale,
   },
+  dateLocale: null,
+  dateLocaleName: 'en',
 };
